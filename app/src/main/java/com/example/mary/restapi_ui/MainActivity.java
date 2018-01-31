1c71061033f1a823142bc8ced7a9eab3c1ed844c
Marina <motroshina@yandex.ru>
2018-01-31 19:22:49 +0300

    create response

---
+++ app/src/main/java/com/example/mary/restapi_ui/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.mary.restapi_ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        textViewResult = (TextView) findViewById(R.id.textView_result);
+
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://jsonplaceholder.typicode.com/posts/1";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.isSuccessful()){
+                    final String result = response.body().string();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            textViewResult.setText(result);
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+}
